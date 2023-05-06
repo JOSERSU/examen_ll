ab7fcfd2c396b5d76ab3f659dff7c6f5613f1cf6
JOSERSU <90009804+JOSERSU@users.noreply.github.com>
2023-05-05 18:07:02 -0600

    exa ll deply

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const hbs = require("hbs");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const puerto = process.env.PORT || 3000;
+
+
+const app = express();
+
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname+'/views/partials', ()=>{})
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json())
+
+app.use(cors())
+
+
+
+//definir rutas
+
+//GET o POST 
+
+
+
+app.get('/productos',(req,res)=>{
+    res.render('table-datatable-basic')
+})
+
+
+
+
+
+app.listen(puerto,()=>{
+    console.log('El servidor esta corriendo en el puerto: ', puerto);
+})
+
